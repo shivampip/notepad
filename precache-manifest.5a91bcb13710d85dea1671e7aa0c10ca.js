f0537ba56638b297df960a34ae540eed33ff20c1
root <root@vps.server.local>
2020-06-23 15:05:20 +0000

    Updates

---
+++ precache-manifest.5a91bcb13710d85dea1671e7aa0c10ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ca6a25fa35d93460a90cd0700033d6",
+    "url": "/notepad/index.html"
+  },
+  {
+    "revision": "281eade2d48c06dce244",
+    "url": "/notepad/static/css/main.231bf734.chunk.css"
+  },
+  {
+    "revision": "1f13fb44893ff4cf1aa5",
+    "url": "/notepad/static/js/2.03c9b2cc.chunk.js"
+  },
+  {
+    "revision": "e53e2677c041eb2118a7597391e3cb96",
+    "url": "/notepad/static/js/2.03c9b2cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "281eade2d48c06dce244",
+    "url": "/notepad/static/js/main.2d75154e.chunk.js"
+  },
+  {
+    "revision": "cae7504569adfb7f181f",
+    "url": "/notepad/static/js/runtime-main.9c8e3f8c.js"
+  }
+]);
